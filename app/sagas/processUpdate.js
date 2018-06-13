6769189899763ac46ad82d1054d7d90c22c6532d
Anima <Vereeckenjamie@gmail.com>
2018-06-13 21:57:43 +0200

    Add MD5 check for corrupt files

--- app/sagas/processUpdate.js
+++ app/sagas/processUpdate.js
@@ -1,9 +1,10 @@
-import { all, call, put, take } from 'redux-saga/effects';
-import * as fs from 'fs';
-import request from 'request';
-import type { UpdateEntry } from '../actions/update';
-import * as actions from '../actions/update';
-import config from '../config';
+import { all, call, put, take } from "redux-saga/effects";
+import * as fs from "fs";
+import request from "request";
+import crypto from "crypto";
+import type { UpdateEntry } from "../actions/update";
+import * as actions from "../actions/update";
+import config from "../config";
 
 // Watcher saga to watch for UPDATE_FOUND actions, runs processUpdateWorker()
 function* processUpdateWatcher() {
@@ -18,9 +19,10 @@ function* processUpdateWorker(action: {
 }) {
   try {
     yield all([...action.payload.map(entry => call(processFile, entry))]);
+    // yield all([...action.payload.slice(0, 1).map(entry => call(processFile, entry))]); // Testing
     yield put(actions.updatesDone());
   } catch (err) {
-    console.log('PROCESS_UPDATE_WORKER ERR', err);
+    console.log("PROCESS_UPDATE_WORKER ERR", err);
   }
 }
 
@@ -29,25 +31,37 @@ function* processFile(entry: UpdateEntry) {
 }
 
 function doSomething(entry) {
-  return new Promise(resolve => {
-    const fileName = entry.path
-      .split('\\')
-      .slice(-1)
-      .pop();
+  return new Promise((resolve, reject) => {
+    const fileName = entry.path.split("\\").slice(-1).pop();
     const saveDirectory = `${process.cwd()}/${config.GAME_FOLDER}/${fileName}`;
     const writer = fs.createWriteStream(saveDirectory);
+    const hash = crypto.createHash("md5");
     const req = request({
-      method: 'GET',
+      method: "GET",
       uri: `${config.API_URL}/${entry.path}`
     });
+    // Pipe the request to the writer
     req.pipe(writer);
-    writer.on('finish', () => {
-      resolve(
-        actions.fileComplete({
-          ...entry,
-          done: true
-        })
-      );
+
+    // Update the hash (digest hash on end and check if file isn't corrupted)
+    req.on("data", data => {
+      // hash.update(`${data}1`); // Test: Corrupt the md5
+      hash.update(data);
+    });
+
+    writer.on("finish", () => {
+      if (hash.digest("hex") === entry.md5) {
+        resolve(
+          actions.fileComplete({
+            ...entry,
+            done: true
+          })
+        );
+      } else {
+        // If hashes aren't equal the file is corrupt, reject the promise
+        // TODO: Add the file to queue again
+        reject();
+      }
     });
   });
 }
