eab81aaaec9e3ae6a60da2bb0b329266e7752b71
Dennis Kievits <denkievits@gmail.com>
2018-06-26 16:25:10 +0200

    Use different boilerplate with ReactJS + TypeScript + Hot Module Replacement and SCSS

--- app/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  API_URL: "http://62.4.16.132:3000",
-  GAME_FOLDER: "LoMGame",
-  LOCAL_UPDATE_FILE: "updates.json"
-};
-
-config.LOCAL_UPDATE_FILE_PATH = `${config.GAME_FOLDER}/${config.LOCAL_UPDATE_FILE}`;
-
-export default config;
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,72 +2,41 @@
  * Base webpack config used across other specific configs
  */
 
-import path from 'path';
-import webpack from 'webpack';
-import fs from 'fs';
-import { dependencies as externals } from './app/package.json';
-import { dependencies as possibleExternals } from './package.json';
+const path = require('path');
+const {
+  dependencies: externals
+} = require('./app/package.json');
 
-// Find all the dependencies without a `main` property and add them as webpack externals
-function filterDepWithoutEntryPoints(dep: string): boolean {
-  // Return true if we want to add a dependency to externals
-  try {
-    // If the root of the dependency has an index.js, return true
-    if (fs.existsSync(path.join(__dirname, `node_modules/${dep}/index.js`))) {
-      return false;
-    }
-    const pgkString = fs
-      .readFileSync(path.join(__dirname, `node_modules/${dep}/package.json`))
-      .toString();
-    const pkg = JSON.parse(pgkString);
-    const fields = ['main', 'module', 'jsnext:main', 'browser'];
-    return !fields.some(field => field in pkg);
-  } catch (e) {
-    console.log(e);
-    return true;
-  }
-}
-
-export default {
-  externals: [
-    ...Object.keys(externals || {}),
-    ...Object.keys(possibleExternals || {}).filter(filterDepWithoutEntryPoints)
-  ],
-
+module.exports = {
   module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            cacheDirectory: true
-          }
-        }
-      }
-    ]
+    loaders: [{
+      test: /\.tsx?$/,
+      loaders: ['react-hot-loader/webpack', 'ts-loader'],
+      exclude: /node_modules/
+    }, {
+      test: /\.json$/,
+      loader: 'json-loader'
+    }]
   },
 
   output: {
     path: path.join(__dirname, 'app'),
+    filename: 'bundle.js',
+
     // https://github.com/webpack/webpack/issues/1114
     libraryTarget: 'commonjs2'
   },
 
-  /**
-   * Determine the array of extensions that should be used to resolve modules.
-   */
+  // https://webpack.github.io/docs/configuration.html#resolve
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-    modules: [path.join(__dirname, 'app'), 'node_modules']
+    extensions: ['.js', '.ts', '.tsx', '.json'],
+    modules: [
+      path.join(__dirname, 'app'),
+      'node_modules',
+    ]
   },
 
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: 'production'
-    }),
+  plugins: [],
 
-    new webpack.NamedModulesPlugin()
-  ]
+  externals: Object.keys(externals || {})
 };
