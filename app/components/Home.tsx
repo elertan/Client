1746648ee5e34365c33bc576096fbb4cc034367c
Anima <Vereeckenjamie@gmail.com>
2018-06-29 12:00:12 +0200

    Add link to login (for development)

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,14 +1,16 @@
-import * as React from 'react';
-import Header from './Home/Header';
+import * as React from "react";
+import Header from "./Home/Header";
+import { Link } from "react-router-dom";
 
-const styles = require('./Home.scss');
+const styles = require("./Home.scss");
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
-          <div className={styles.container} data-tid="container">
-          <Header />
+        <div className={styles.container} data-tid="container">
+          <Header/>
+          <Link to="/login">go to login</Link>
           {/* <div className={styles.tempBackground}></div> */}
         </div>
       </div>
